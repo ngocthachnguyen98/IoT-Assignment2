61617f65f30325cec8bb885569fc241fb7c5c7e0
vinhngogia0906 <vinhngogia0906@gmail.com>
2020-04-19 07:04:41 +1000

    index.php authenticate user with mysql

--- MP/index.php
+++ MP/index.php
@@ -2,14 +2,30 @@
     Put this file in your /var/www/html/ directory on your MP
  -->
  <?php
-    
+    $message="";
+    if(isset($_POST['submit'])){
+        $connect = mysqli_connect("localhost","root","pynative@#29","carshare");
+        $result = mysqli_query($connect, "SELECT * FROM Users WHERE username = '".$_POST["username"]."' and password = '".$_POST["password"]."'");
+        $count = mysqli_num_rows($result);
+        if ($count == 0){
+            $message = "Invalid Login Detail !";
+        }
+        else {
+            $message = "Logged in successfully !";
+        }
+    }
  ?>
  <html>
     <body>
     <form action = "index.php" method = "post">
-        Username: <input type = "text" name="user" /><br>
+        Username: <input type = "text" name="username" /><br>
         Password: <input type="password" name="password"/><br>
         <input type="submit" value="Log In" />
+        <?php
+            if ($message!= ""){
+                echo $message;
+            }
+        ?>
     </form>
     </body>
  </html>
