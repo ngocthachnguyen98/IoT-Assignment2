fce3763e78c4ef2a7a566fd3f9e492a0560dfce5
vinhngogia0906 <vinhngogia0906@gmail.com>
2020-04-19 07:09:16 +1000

    index.php authenticate user with mysql

--- MP/index.php
+++ MP/index.php
@@ -1,20 +1,6 @@
 <!-- On MP set up, I use apache2 to run the server and php7 as well as phpmyadmin to mange the Mysql DB
     Put this file in your /var/www/html/ directory on your MP
  -->
- <?php
-    $message="";
-    if(isset($_POST['submit'])){
-        $connect = mysqli_connect("localhost","root","pynative@#29","carshare");
-        $result = mysqli_query($connect, "SELECT * FROM Users WHERE username = '".$_POST["username"]."' and password = '".$_POST["password"]."'");
-        $count = mysqli_num_rows($result);
-        if ($count == 0){
-            $message = "Invalid Login Detail !";
-        }
-        else {
-            $message = "Logged in successfully !";
-        }
-    }
- ?>
  <html>
     <body>
     <form action = "index.php" method = "post">
@@ -22,6 +8,18 @@
         Password: <input type="password" name="password"/><br>
         <input type="submit" value="Log In" />
         <?php
+            $message="";
+            if(isset($_POST['submit'])){
+                $connect = mysqli_connect("localhost","root","pynative@#29","carshare");
+                $result = mysqli_query($connect, "SELECT * FROM Users WHERE username = '".$_POST["username"]."' and password = '".$_POST["password"]."'");
+                $count = mysqli_num_rows($result);
+                if ($count == 0){
+                    $message = "Invalid Login Detail !";
+                }
+                else {
+                    $message = "Logged in successfully !";
+                }
+            }
             if ($message!= ""){
                 echo $message;
             }
